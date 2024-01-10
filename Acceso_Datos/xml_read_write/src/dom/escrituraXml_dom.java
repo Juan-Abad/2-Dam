179ea8d21a29fc89aecaaf4d985262ab8b328867
Juan <juan.abad.hernandez@gmail.com>
2024-01-10 21:04:00 +0100

    acceso a datos, ejercicios xml(DOM, SAX), empezando

---
+++ Acceso_Datos/xml_read_write/src/dom/escrituraXml_dom.java
@@ -0,0 +1,10 @@
+package dom;
+
+public class escrituraXml_dom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Acceso_Datos/xml_read_write/src/dom/lecturaXml_dom.java
@@ -0,0 +1,10 @@
+package dom;
+
+public class lecturaXml_dom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
