39a1228e6c407df8bad3e8a3c078dfbe6930a077
Juan <juan.abad.hernandez@gmail.com>
2024-02-11 14:24:46 +0100

    actualizando proyectos

--- desarrollo_de_interfaces_entregaUD6/helpset.hs
+++
@@ -1,8 +0,0 @@
-<!-- helpset.hs -->
-<?xml version='1.0' encoding='UTF-8'?>
-<!DOCTYPE helpset
-    PUBLIC  "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
-            "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
-<helpset version="2.0">
-    <
-</helpset>
